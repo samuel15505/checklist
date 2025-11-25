d54d7f117a9835493b86671df9363ba522bd6ca5
samuel15505 <sbanwell@free.fr>
2025-11-25 23:33:35 +0100

    add skeleton for checklist

--- src/Checklist.zig
+++ src/Checklist.zig
@@ -1,10 +1,28 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 const check_items = @import("items/items.zig");
 
 name: []const u8,
 items: std.ArrayList(Item),
+allocator: Allocator,
 
 const Item = union(enum) {
     challenge: check_items.Challenge,
     text: check_items.SimpleText,
 };
+
+pub fn init(gpa: Allocator, name: []const u8) !@This() {
+    return .{
+        .name = try gpa.dupe(u8, name),
+        .items = .empty,
+        .allocator = gpa,
+    };
+}
+
+pub fn deinit(self: @This()) void {
+    for (self.items) |item| {
+        item.deinit(self.allocator);
+    }
+    self.items.deinit(self.allocator);
+    self.allocator.free(self.name);
+}
