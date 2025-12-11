117bacbf26facc00d1dd7b12fb2f403bc6a59a0f
samuel15505 <sbanwell@free.fr>
2025-12-11 14:50:52 +0100

    add basic validation

--- src/root.zig
+++ src/root.zig
@@ -7,50 +7,95 @@ pub const CheckFile = struct {
 };
 
 pub const Groups = struct {
-    groups: []Group,
+    groups: std.ArrayList(Group) = .empty,
 };
 
 pub const Group = struct {
     name: []const u8,
-    checklists: []Checklist,
+    checklists: std.ArrayList(Checklist) = .empty,
 };
 
 pub const Checklist = struct {
     name: []const u8,
-    items: []Item,
+    items: std.ArrayList(Item) = .empty,
 };
 
-pub const Item = struct {
-    item_type: ItemType,
-    text: []const u8,
-    comment: []const u8,
+pub const Item = union(enum) {
+    challenge: *Challenge,
+    text: *Text,
+};
+
+const Challenge = struct {
+    title: []const u8,
+    description: []const u8,
     response: []const u8,
 
-    pub fn new(allocator: Allocator, item_type: ItemType, text: []const u8, comment: []const u8, response: []const u8) !@This() {
-        const fields = [_]std.ArrayList(u8){.empty} ** 3;
-        errdefer {
-            for (fields) |field| {
-                field.deinit(allocator);
-            }
+    pub fn new(allocator: Allocator, title: []const u8, description: []const u8, response: []const u8) !Challenge {
+        var challenge: Challenge = undefined;
+        challenge.title = try allocator.dupe(u8, title);
+        errdefer allocator.free(challenge.title);
+
+        challenge.description = try allocator.dupe(u8, description);
+        errdefer allocator.free(challenge.description);
+
+        challenge.response = try allocator.dupe(u8, response);
+
+        return challenge;
+    }
+
+    pub fn deinit(self: *@This(), allocator: Allocator) void {
+        for (@typeInfo(@This()).@"struct".fields) |field| {
+            allocator.free(@field(self, field.name));
         }
+    }
 
-        try fields[0].appendSlice(allocator, text);
-        try fields[1].appendSlice(allocator, comment);
-        try fields[2].appendSlice(allocator, response);
+    pub fn validate(self: *const @This()) bool {
+        return self.title.len <= 40 and self.description.len <= 100 and self.response.len <= 40;
+    }
 
+    pub fn trySetTitle(self: *@This(), allocator: Allocator, title: []const u8) !void {
+        if (title.len <= 40) {
+            self.title = try allocator.dupe(u8, title);
+        } else {
+            return error.TooLong;
+        }
+    }
+
+    pub fn trySetDescription(self: *@This(), allocator: Allocator, description: []const u8) !void {
+        if (description.len <= 100) {
+            self.description = try allocator.dupe(u8, description);
+        } else {
+            return error.TooLong;
+        }
+    }
+
+    pub fn trySetResponse(self: *@This(), allocator: Allocator, response: []const u8) !void {
+        if (response.len <= 40) {
+            self.response = try allocator.dupe(u8, response);
+        } else {
+            return error.TooLong;
+        }
+    }
+};
+
+const Text = struct {
+    item_type: ItemType,
+    text: []const u8,
+
+    pub fn new(allocator: Allocator, item_type: ItemType, text: []const u8) !Text {
         return .{
             .item_type = item_type,
-            .text = try fields[0].toOwnedSlice(allocator),
-            .comment = try fields[1].toOwnedSlice(allocator),
-            .response = try fields[2].toOwnedSlice(allocator),
+            .text = try allocator.dupe(u8, text),
         };
     }
 
     pub fn deinit(self: *@This(), allocator: Allocator) void {
         allocator.free(self.text);
-        allocator.free(self.comment);
-        allocator.free(self.response);
     }
+
+    // pub fn validate(self: *@This()) bool {
+
+    // }
 };
 
 pub const ItemType = enum {
